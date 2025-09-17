c1c09203a7430ba37b19e01d89e1507af60083c0
Phantu2012 <Phantu2012@gmail.com>
2025-09-18 00:43:28 +0700

    feat: Update Firebase project configuration
    Changes the `authDomain`, `projectId`, and `storageBucket` to reflect the new Firebase project setup for the YouTube Video Hub. This ensures correct initialization and connection to the production environment.

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -31,10 +31,10 @@
 
 export const firebaseConfig = {
   apiKey: "AIzaSyAUnmo1FRReKnMpHqHB1jVJD5CdfGA4x2E",
-  authDomain: "video-hub-1aabc.firebaseapp.com",
-  projectId: "video-hub-1aabc",
-  storageBucket: "video-hub-1aabc.firebasestorage.app",
+  authDomain: "youtube-hub-2025.firebaseapp.com",
+  projectId: "youtube-hub-2025",
+  storageBucket: "youtube-hub-2025.firebasestorage.app",
   messagingSenderId: "1017358933371",
   appId: "1:1017358933371:web:7ea24fae4b72aab06771de",
   measurementId: "G-439MDT7ETH"
-};  
+};
